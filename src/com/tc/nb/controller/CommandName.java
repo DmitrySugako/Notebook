60109229d1bcfeef644350e74d34c2ccb5476f96
DmitrySugako <113543432+DmitrySugako@users.noreply.github.com>
2023-01-11 22:21:12 +0300

    Delete src directory

--- src/com/tc/nb/controller/CommandName.java
+++
@@ -1,5 +0,0 @@
-package com.tc.nb.controller;
-
-public enum CommandName {
-	CONTENT_SEARCH, SEARCH_BY_DATE, ADDING_RECORDS, WRONG_REQUEST;
-}
--- src/com/tc/nb/controller/command/Command.java
+++
@@ -1,6 +0,0 @@
-package com.tc.nb.controller.command;
-
-public interface Command {
-	public String execute(String request);
-
-}
